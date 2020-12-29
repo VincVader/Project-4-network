2b7b3947780485b130a7b2a7251630f9ed943b29
VincVader <vincvader@mail.ru>
2020-12-29 03:09:01 +0300

    Create Post Model and index page

--- project4/network/models.py
+++ project4/network/models.py
@@ -1,6 +1,15 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 
 class User(AbstractUser):
     pass
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(max_length=255)
+    date_posted = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return f"ID: {self.pk} Author: {self.author} Posted at: {self.date_posted}."
